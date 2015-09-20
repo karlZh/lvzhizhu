64a0e498f1a455c39723737e31445583768373f4
lamplijie <86267659@qq.com>
2015-09-20 23:31:21 +0800

    add

--- protected/controllers/IndexController.php
+++ protected/controllers/IndexController.php
@@ -1,5 +1,21 @@
 <?php
+    /*
+     * 前台控制器
+     * @author lamplijie<www.lamplijie.com>
+     * @date 2015-9-10
+     * @time 10:30
+     * @since v1.0
+     */
 	class IndexController extends CController{
+
+        /*
+         * actionIndex
+         * 前台首页渲染操作
+         * @author lamplijie<www.lamplijie.com>
+         * @date 2015-9-10
+         * @time 10:30
+         * @since v1.0
+         */
 		public function actionIndex(){
 			$this->render('index');
 		}
